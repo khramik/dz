bcec805e2d325af3b81fa6ef8f47c1940da2e7f7
khramik <igor.khramik@gmail.com>
2022-01-10 17:34:36 +0200

    Create dz_1.py
    1 домашние задание

---
+++ dz_1.py
@@ -0,0 +1,48 @@
+print('######')
+print('#    #')
+print('#    #')
+print('#    #')
+print('######')
+
+
+print('#    #')
+print('#    #')
+print('######')
+print('#    #')
+print('#    #')
+
+
+
+
+print('We all live in a yellow submarine')
+print('Yellow submarine, yellow submarine')
+print('We all live in a yellow submarine')
+print('Yellow submarine, yellow submarine')
+print('')
+print('''We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine''')
+
+
+name='Игорь'
+surname='Храмов'
+print(surname, name)
+
+start_string = "abcdefghijklmn"
+new_string_first=start_string[:4]
+new_string_reverse=start_string[-5:]
+new_string_mid=start_string[4:8]
+every_second=start_string[1::2]
+print(new_string_first)
+print(new_string_reverse)
+print(new_string_mid)
+print(every_second)
+
+x=3
+y=2
+
+print(f'сложение {x}+{y}={x+y}')
+print(f'вычетание{x}-{y}={x-y}')
+print(f'умножение{x}*{y}={x*y}')
+print(f'деление{x}/{y}={x/y}')
